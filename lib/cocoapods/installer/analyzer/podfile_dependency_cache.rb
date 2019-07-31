e86848ad91ece5d3b33050192b3d15d058714687
Eric Amorde <eric.amorde@gmail.com>
2019-07-31 00:36:53 -0700

    [Analyzer] Do not implicitly whitelist transitive dependencies
    Closes #6838
    
    This resolves an issue which caused the transitive dependencies of a dependency with a whitelist (ex. `:configurations => ['Debug']`)
    to be linked with the host target even for configurations not included in the whitelist.
    
    This should also provide a small build time improvement for projects using the new build system that contain these dependencies,
    when using recent versions of Xcode that detect implicit dependencies based on linker flags (Xcode 10.2 or higher).

--- lib/cocoapods/installer/analyzer/podfile_dependency_cache.rb
+++ lib/cocoapods/installer/analyzer/podfile_dependency_cache.rb
@@ -10,22 +10,27 @@ module Pod
         #
         attr_reader :podfile_dependencies
 
+        # @param [Array<Pod::Dependency>] podfile_dependencies @see #podfile_dependencies
+        #
+        # @param [Hash{Pod::TargetDefinition => Array<Pod::Dependency>}] dependencies_by_target_definition
+        #        dependencies keyed by their target definition
+        #
         def initialize(podfile_dependencies, dependencies_by_target_definition)
           @podfile_dependencies = podfile_dependencies
           @dependencies_by_target_definition = dependencies_by_target_definition
         end
 
-        # Returns the dependencies for the given target definition
+        # @return [Array<Pod::Dependency>] The dependencies for the given target definition
         #
         def target_definition_dependencies(target_definition)
-          @dependencies_by_target_definition[target_definition] ||
+          dependencies_by_target_definition[target_definition] ||
             raise(ArgumentError, "dependencies for #{target_definition.inspect} do not exist in the cache")
         end
 
-        # Returns a list of all of the target definitions in the Podfile
+        # @return [Array<Pod::TargetDefinition>] A list of all of the target definitions in the Podfile
         #
         def target_definition_list
-          @dependencies_by_target_definition.keys
+          dependencies_by_target_definition.keys
         end
 
         # Creates a {PodfileDependencyCache} from the given {Podfile}
@@ -49,6 +54,13 @@ module Pod
 
           new(podfile_dependencies.freeze, dependencies_by_target_definition.freeze)
         end
+
+        private
+
+        # @return [Hash{TargetDefinition => Array<Pod::Dependency>}]
+        #         dependencies keyed by their target definition
+        #
+        attr_reader :dependencies_by_target_definition
       end
     end
   end
